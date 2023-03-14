af36380002ac2fe2dcbf7ad0d7c98c76c5650007
jueshan <jueshancoder@gmail.com>
2023-03-14 18:38:04 +0800

    commit some code for Uniswap v2 part2.

--- uniswap/v2/src/ZuniswapV2Pair.sol
+++ uniswap/v2/src/ZuniswapV2Pair.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 
 import "solmate/tokens/ERC20.sol";
 import "./libraries/Math.sol";
+import "./libraries/UQ112x112.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -11,9 +12,14 @@ interface IERC20 {
 
 error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
+error InsufficientOutputAmount();
+error InsufficientLiquidity();
+error InvalidK();
 error TransferFailed();
+error BalanceOverflow();
 
 contract ZuniswapV2Pair is ERC20, Math{
+    using UQ112x112 for uint224;
     uint256 constant MINIMUM_LIQUIDITY = 1000;
 
     address public token0;
@@ -22,8 +28,14 @@ contract ZuniswapV2Pair is ERC20, Math{
     uint112 private reserve0;
     uint112 private reserve1;
 
+    uint32 private blockTimestampLast;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+
     event Burn(address indexed sender, uint256 amount0, uint256 amount1);
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Swap(address indexed sender, uint256 amount0Out, uint256 amount1Out, address indexed to);
     event Sync(uint256 reserve0, uint256 reserve1);
 
     constructor(address token0_, address token1_) ERC20("ZUniswpV2 Pair","ZUNIV2", 18) {
@@ -86,6 +98,34 @@ contract ZuniswapV2Pair is ERC20, Math{
         emit Burn(msg.sender, amount0, amount1);
     }
 
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to
+    ) public {
+        if (amount0Out == 0 && amount1Out == 0)
+            revert InsufficientOutputAmount();
+
+        (uint256 reserve0_, uint256 reserve1_,) = getReserves();
+
+        if (amount0Out > reserve0_ || amount1Out > reserve1_)
+            revert InsufficientLiquidity();
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        if (balance0 * balance1 < uint256(reserve0) * uint256(reserve1))
+            revert InvalidK();
+
+        _update(balance0, balance1, reserve0, reserve1);
+
+        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+
+    }
+
     function sync() public {
         _update(
             IERC20(token0).balanceOf(address(this)),
@@ -105,9 +145,28 @@ contract ZuniswapV2Pair is ERC20, Math{
         return (reserve0, reserve1, 0);
     }
 
-    function _update(uint256 balance0, uint256 balance1) private {
+    function _update(
+        uint256 balance0,
+        uint256 balance1,
+        uint256 reserve0_,
+        uint256 reserve1_
+    ) private {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max)
+            revert BalanceOverflow();
+
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if (timeElapsed > 0  && reserve0 > 0 && reserve1 > 0) {
+                price0CumulativeLast += uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) * timeElapsed;
+
+                price1CumulativeLast += uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1)) * timeElapsed;
+            }
+        }
+
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
 
         emit Sync(reserve0, reserve1);
     }
